b4e45c1fa86fc96dd2d6f96c51df9a94e58d01b7
Elliott Leonard <egleonard88@gmail.com>
2022-04-18 16:25:35 -0600

    finished lab

--- delete.sql
+++ delete.sql
@@ -1,10 +1,26 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+--2.
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+
+--4.
+delete
+from animals
+where type = 'lion'
+
+--5. 
+delete
+from animals
+where name like 'M%'
+
+--6. 
+delete
+from animals
+where age < 9
